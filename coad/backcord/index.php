d5385b5eb2f32e4cdc17364b38bc484409bbae34
fukutomi <tixyovi@hotmail.co.jp>
2015-01-27 10:41:45 +0900

    バックエンド側 ログインとメニュー

---
+++ coad/backcord/index.php
@@ -0,0 +1,93 @@
+﻿<!-- HTML5で開発します -->
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+
+		<meta charset="UTF-8">
+
+		<!-- ↓ここには全ページ共通のcss↓ -->
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/common.css">
+		<!-- ↑ここには全ページ共通のcss↑ -->
+
+		<!-- ↓ここにはそのページ用のcss↓ -->
+		<link rel="stylesheet" href="css/index.css">
+		<!-- ↑ここにはそのページ用のcss↑ -->
+
+		<title>管理者ログイン | HALシネマ</title>
+
+		<?php
+			error_reporting(E_ALL & ~E_NOTICE);
+			$message = "";
+			//ログイン押した後の処理書くよ
+			if( isset( $_POST["id"] ) && isset( $_POST["pass"] ) )
+			{
+				$id = $_POST["id"];
+				$pass = $_POST["pass"];
+				$connect = mysqli_connect("localhost","root","");
+				mysqli_select_db($connect,"hal_cinema");
+				mysqli_set_charset($connect,"utf8");
+				$sql = "SELECT count(*) as cnt FROM administrator WHERE id = '".$id."' AND password = '".$pass."'";
+				$result = mysqli_query($connect,$sql);
+				while( $data = mysqli_fetch_array($result) )
+				{
+					if( $data['cnt'] == 1 )
+					{
+						header("Location:menu.php");
+					}
+					else
+					{
+						$message = "IDかパスワードが違います";
+					}
+				}
+			}
+		?>
+
+	</head>
+
+	<body>
+
+		<div id="wrapper">
+			
+			<div id="main">
+
+				<h1>管理者ログイン</h1>
+
+				<div id="maininner">
+
+					<form action="index.php" method="post">
+						<?php echo "<span id='loginerror'>".$message."</span><br>"; ?>
+						ID<input type="text" name="id" id="loginid"><br><br>
+						PASS<input type="password" name="pass" id="loginpass"><br><br>
+						<input type="submit" value="ログイン" id="loginbutton">
+
+					</form>
+
+				</div>
+
+			</div>
+
+			<div id="menu">
+
+				<h1>メニュー</h1>
+
+				<div id="menuinner">
+					
+					<ul>
+						<li>トップページ</li>
+						<li>映画情報登録・編集</li>
+						<li>上映スケジュール登録・編集</li>
+						<li>月別予約状況PDF出力</li>
+					<ul>
+					
+				</div>
+
+			</div>
+			
+		</div>
+
+	</body>
+
+</html>
---
+++ coad/backcord/template.php
@@ -0,0 +1,60 @@
+﻿<!-- HTML5で開発します -->
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+
+		<meta charset="UTF-8">
+
+		<!-- ↓ここには全ページ共通のcss↓ -->
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/common.css">
+		<!-- ↑ここには全ページ共通のcss↑ -->
+
+		<!-- ↓ここにはそのページ用のcss↓ -->
+		<link rel="stylesheet" href="css/.css">
+		<!-- ↑ここにはそのページ用のcss↑ -->
+
+		<title> | HALシネマ</title>
+
+	</head>
+
+	<body>
+
+		<div id="wrapper">
+			
+			<div id="main">
+
+				<h1>管理者○○○</h1>
+
+				<div id="maininner">
+
+					ここに処理とか書いて
+
+				</div>
+
+			</div>
+
+			<div id="menu">
+
+				<h1>メニュー</h1>
+
+				<div id="menuinner">
+					
+					<ul>
+						<li><a href="#menu.php">トップページ</a></li>
+						<li><a href="#moviedata_edit.php">映画情報登録・編集</a></li>
+						<li><a href="#schedule_edit.php">上映スケジュール登録・編集</a></li>
+						<li><a href="#output_pdf.php">月別予約状況PDF出力</a></li>
+					<ul>
+					
+				</div>
+
+			</div>
+			
+		</div>
+
+	</body>
+
+</html>
