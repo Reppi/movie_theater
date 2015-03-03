683f6777e76c8c85c7d658bf94b4e315fe9c0898
blanket11 <info@cheekpouch.com>
2015-03-03 15:37:44 +0900

    pdf更新

--- coad/backcord/pdf.php
+++ coad/backcord/pdf.php
@@ -1,8 +1,8 @@
 <?php
 	error_reporting(E_ALL & ~E_NOTICE);
-	//‚±‚±‚©‚ç‚Í‚è‚Â‚¯‚é
+	//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Í‚ï¿½ï¿½Â‚ï¿½ï¿½ï¿½
 
-	//POSTó‚¯æ‚è
+	//POSTï¿½ó‚¯ï¿½ï¿½ï¿½
     if(isset($_POST["month"])){
        $month = $_POST["month"];
     }
@@ -11,7 +11,7 @@
 	$dbms_user = "user";
 	$dbms_pass = "user";
 
-	$con = mysql_connect($host_name,$dbms_user,$dbms_pass);
+	$con = @mysql_connect($host_name,$dbms_user,$dbms_pass);
 	mysql_query("SET NAMES utf8");
 
 	mysql_select_db("hal_cinema",$con);
@@ -19,7 +19,7 @@
 	//reserved_time SQL
 	//$sql = "SELECT reserved_time as rtime FROM ticket_reserved";
 
-	//ƒ`ƒPƒbƒgSQL
+	//ï¿½`ï¿½Pï¿½bï¿½gSQL
 	//$sql = "SELECT * FROM ticket_reserved WHERE ".substr($reserved_time, 6, 2)."reserved_time";
 
 	$sql = "SELECT COUNT(reserved_movieid) AS movie FROM ticket_reserved WHERE DATE_FORMAT(reserved_time, '%m')=$month GROUP BY reserved_movieid";
@@ -30,9 +30,9 @@
 		$booking_human[$cnt] = $reserved["movie"];
 		$cnt++;
 	}
-	//ƒ^ƒCƒgƒ‹SQL
+	//ï¿½^ï¿½Cï¿½gï¿½ï¿½SQL
 	$sql = "SELECT movie.movie_title FROM ticket_reserved INNER JOIN movie ON ticket_reserved.reserved_movieid = movie.movie_id GROUP BY movie_title";
-	//SQLÀs
+	//SQLï¿½ï¿½ï¿½s
 	$result = mysql_query($sql,$con);
 	$cnt = 0;
 	$movie_title = array();
@@ -49,19 +49,19 @@
 
 	$pdf -> addMbFont(PMINCHO,'SJIS');
 	$pdf -> setFont(PMINCHO,'',16);
-	$pdf -> write( 30 , $month."Œ‚Ì—\–ñ‚Í" );
+	$pdf -> write( 30 , $month."ï¿½ï¿½ï¿½Ì—\ï¿½ï¿½ï¿½ï¿½" );
 	$pdf -> Ln( 20 );
 
 	for($i=0; $i<$cnt; $i++){
 	$movie_title[$i] = mb_convert_encoding($movie_title[$i],"SJIS");
 
 	$pdf -> Ln( 20 );
-	$pdf -> write( 20 , $movie_title[$i]."‚Ì—\–ñ”‚Í@" );
+	$pdf -> write( 20 , $movie_title[$i]."ï¿½Ì—\ï¿½ñ”‚Í@" );
 	if($booking_human[$i] < 1 ){
 	$booking_human[$i] = 0;
 	}
-	$pdf -> write( 20 , $booking_human[$i]."Œ‚Å‚·" );
+	$pdf -> write( 20 , $booking_human[$i]."ï¿½ï¿½ï¿½Å‚ï¿½" );
 }
 
-	$pdf -> output("test.pdf","I");	
+	$pdf -> output("test.pdf","I");
 ?>
