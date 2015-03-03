9634f9db1b52ecb3f09d3f3146d3fbf05d14e765
N.Tomatsu <tomatsu.norimichi@gmail.com>
2015-03-03 14:29:29 +0900

    完成！

--- coad/js/schedule.js
+++ coad/js/schedule.js
@@ -4,16 +4,14 @@ $(function(){
 
 	$(".week").click(function(){
 		var dayplus = $(this).attr("id");
-		$("#slider").slideUp("slow", function(){
-			//インマスクにデータを送り見えない状態にする
 			$("#inmask").val(dayplus);	
 			$('#mask').submit();
-		});
+	
 
 	});
 
-	$(".time ul li").click(function(){
-		$("#go_ticket").submit();
+	$(".gogo").click(function(){
+		$(".go_ticket").submit();
 	});
 
 });
